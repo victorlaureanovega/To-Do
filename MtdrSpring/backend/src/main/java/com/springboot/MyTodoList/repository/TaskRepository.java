0274d6ef709d473a95f2c80f014a8eb4d78a65fc
victorlaureanovega <victorlaureanovega@gmail.com>
2026-04-13 19:40:09 -0600

    Endpoint para obtener el promedio de horas trabajadas por cada desarrollador del equipo (que haya registrado tareas en Task)

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/TaskRepository.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package com.springboot.MyTodoList.repository;
 import com.springboot.MyTodoList.model.Task;
 import com.springboot.MyTodoList.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -13,9 +15,16 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     // Para que el bot pueda listar las tareas del usuario que pregunta
     List<Task> findByUser_TelegramId(Long telegramId);
 
-    // Buscar tareas por estado (ej: 'Pendiente', 'En curso')
-    List<Task> findByTaskStatus(String status);
+    // Buscar tareas por estado (ej: 'Pendiente', 'En curso') y si están activas o no
+    List<Task> findByTaskStatusAndIsActive(String status, Integer isActive);
 
     // Buscar tareas activas de un usuario
     List<Task> findByUserAndIsActive(User user, Integer isActive);
+
+    // Promedio de horas trabajadas por cada miembro de un equipo (sólo está considerando a los usuarios que tienen tareas registradas con su id en Task)
+    @Query("SELECT SUM(t.totalHoursWorked) / COUNT(DISTINCT u.userId) " +
+       "FROM Task t " +
+       "JOIN t.user u " +
+       "WHERE u.team.teamId = :teamId")
+    Float getAverageWorkedHoursByTeam(@Param("teamId") Long teamId);
 }
