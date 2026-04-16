c7d95db54aba10d4e811242f35d7d10d7bf99bb5
eugeguzmanv <eugegv03@gmail.com>
2026-04-16 15:07:46 -0600

    modified terraform files

--- MtdrSpring/terraform/containerengine.tf
+++ MtdrSpring/terraform/containerengine.tf
@@ -8,7 +8,7 @@ resource "oci_containerengine_cluster" "mtdrworkshop_cluster" {
     ]
     subnet_id = oci_core_subnet.endpoint.id
   }
-  kubernetes_version  = "v1.35.0"
+  kubernetes_version  = "v1.34.2"
   name                = "mtdrworkshopcluster-${var.mtdrKey}"
   vcn_id              = oci_core_vcn.okevcn.id
   #optional
@@ -36,7 +36,7 @@ resource "oci_containerengine_node_pool" "oke_node_pool" {
   #Required
   cluster_id         = oci_containerengine_cluster.mtdrworkshop_cluster.id
   compartment_id     = var.ociCompartmentOcid
-  kubernetes_version = "v1.35.0"
+  kubernetes_version = "v1.34.2"
   name               = "Pool"
   #node_shape        = "VM.Standard.A1.Flex"  #Always Free Option
   node_shape         = "VM.Standard.E3.Flex"
