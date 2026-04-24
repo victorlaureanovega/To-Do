ff6dbec9e6acb9140d893b0ba8491f18814821b6
eugeguzmanv <eugegv03@gmail.com>
2026-04-24 10:47:46 -0600

    changed backend toute

--- MtdrSpring/backend/src/main/frontend/vite.config.js
+++ MtdrSpring/backend/src/main/frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const backendTarget = process.env.VITE_BACKEND_PROXY_TARGET || 'http://localhost:8080'
+const backendTarget = process.env.VITE_BACKEND_PROXY_TARGET || '/'
 
 // https://vite.dev/config/
 export default defineConfig({
